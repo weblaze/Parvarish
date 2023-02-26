dafb9deabc657cfc0e0f7c939303ee130f38da86
Arunav ganguly <63651649+weblaze@users.noreply.github.com>
2023-02-26 20:17:49 +0530

    final structure complete

---
+++ scripts.js
@@ -0,0 +1,9 @@
+bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: true,
+    duration: 600,
+    autoplaySpeed: 15000,
+    breakpoints: [{ changePoint: 480, slidesToShow: 1, slidesToScroll: 1 }, { changePoint: 640, slidesToShow: 1, slidesToScroll: 1 }, { changePoint: 768, slidesToShow: 1, slidesToScroll: 1 } ],
+  });
